ac8717f50c8e895fe3a9ad34386ea26b6391ff08
Artsiom <artem.kuvik.93@mail.ru>
2024-09-03 11:29:27 +0300

    feat: add menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Menu } from "./pages/Menu/Menu.tsx";
